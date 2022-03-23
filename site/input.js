e5024647c75cc107f28937ba69ef4a5e54df2374
MeechaPooch <powchm@catlin.edu>
2022-03-23 00:22:03 -0700

    cluckable buttons

--- site/input.js
+++ site/input.js
@@ -9,3 +9,12 @@ function incrCounter(label,ammount) {
     data[label] += ammount
     document.querySelector('.inlineNum[data-type=' + label + ']').innerText=data[label]
 }
+///// CHECKBOX /////
+
+function selectOption(box) {
+    box.classList.toggle('selected')
+}
+
+document.querySelectorAll('.checkboxBox').forEach(boxbox=>{
+    boxbox.onclick = ()=>{selectOption(boxbox)}
+})
