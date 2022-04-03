09519390923a0770cd340edeaa8599e94fddc751
MeechaPooch <powchm@catlin.edu>
2022-04-02 17:23:28 -0700

    try to fix caching...

--- site/file/service-worker.js
+++ site/file/service-worker.js
@@ -1,16 +1,22 @@
+let statics =  [
+  '/',
+  '/file/style.css',
+  '/file/script.js',
+  '/file/state.js',
+  '/file/input.js',
+  '/file/manifest.json',
+  '/file/fonts/gotham/Gotham-Bold.otf',
+  '/img/coopscout_circle.png',
+  '/img/coopscout.png',
+  '/file/register-service-worker.js',
+  '/file/qrcode.min.js',
+]
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('coopscout').then(function(cache) {
         return cache.addAll(
-          [
-            '/',
-            '/file/style.css',
-            '/file/script.js',
-            '/file/state.js',
-            '/file/input.js',
-            '/file/manifest.json',
-            '/file/fonts/gotham/Gotham-Bold.otf'
-          ]
+          statics
         );
       })
     );
@@ -18,8 +24,8 @@ self.addEventListener('install', function(event) {
 
   self.addEventListener('fetch', function(event) {
     event.respondWith(
-      fetch(event.request).catch(function() {
-        return caches.match(event.request);
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
       })
     );
   });
